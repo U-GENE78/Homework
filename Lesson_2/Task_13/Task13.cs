ddf85b03abd991931df69993de0cac75b67b1d63
Eugene B <e.balyabin@gmail.com>
2022-10-07 20:43:10 +0300

    task 13 is solved

---
+++ Lesson_2/Task_13/Task13.cs
@@ -0,0 +1,27 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+//что третьей цифры нет.
+
+string msg;
+
+Console.Clear();
+
+Console.Write("Enter a number: ");
+int number = int.Parse(Console.ReadLine()!);
+
+if (number > 99)
+{
+    int numberCheck = number;
+    
+    while (numberCheck >= 1000)
+    {
+        numberCheck /= 10;
+    }
+
+    msg = $"The third digit of {number} is {numberCheck % 10}";
+}
+else
+{
+    msg = $"The number is too small";
+}
+
+Console.Write($"\n{msg}");
