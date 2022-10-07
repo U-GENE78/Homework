4a3bf0c24ae4c22dfa99163c6831ec293fe87b38
Eugene B <e.balyabin@gmail.com>
2022-10-07 20:23:59 +0300

    task 10 is solved

---
+++ Lesson_2/Task_10/Task10.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+
+int number;
+
+do
+{
+    Console.Write("Enter a THREE digit number: ");
+    number = int.Parse(Console.ReadLine()!);
+}
+while (number < 100 || number > 999);
+
+Console.Write($"\nThe second digit of {number} is {(number / 10) % 10}");
+
