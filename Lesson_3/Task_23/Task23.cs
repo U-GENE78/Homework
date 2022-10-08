d38c918d7676ee883c1deeab81672ca828b28a6e
Eugene B <e.balyabin@gmail.com>
2022-10-08 12:50:30 +0300

    task 23 added

---
+++ Lesson_3/Task_23/Task23.cs
@@ -0,0 +1,26 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+void PrintCubes(int number)
+{
+
+    Console.WriteLine($"\nNUMBERS\t |\tCUBES");
+    Console.WriteLine();
+
+    for (int i = 1; i <= number; i++)
+    {
+        Console.WriteLine($"{i}\t |\t {i * i * i}");
+    }
+}
+
+Console.Clear();
+
+int number;
+
+do
+{
+    Console.Write("Enter a POSITIVE number: ");
+    number = int.Parse(Console.ReadLine()!);
+}
+while (number < 1);
+
+PrintCubes(number);
