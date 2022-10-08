b58df4a35de8485e191a2378d0abf0215a803de9
Eugene B <e.balyabin@gmail.com>
2022-10-08 11:59:43 +0300

    task 19 added

---
+++ Lesson_3/Task_19/Task19.cs
@@ -0,0 +1,60 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+//является ли оно палиндромом.
+
+//Решение приведено для общего случая
+
+int NumberOfDigits(int num) // считаем количество цифр
+{
+    int dig = 0;
+
+    while (num >= 1)
+    {
+        num /= 10;
+        dig++;
+    }
+
+    return dig;
+}
+
+bool IsPalindrome(int number)
+{
+    int firstDigit;
+    int lastDigit;
+    int powOfTen;
+
+    int numOfDigits = NumberOfDigits(number);
+
+    while (numOfDigits > 1)
+    {
+        powOfTen = (int)Math.Pow(10, numOfDigits - 1);
+
+        firstDigit = number / powOfTen;
+        lastDigit = number % 10;
+
+        if (firstDigit != lastDigit) return false;
+
+        number = (number - firstDigit * powOfTen) / 10;
+        numOfDigits = numOfDigits - 2;
+    }
+
+    return true;
+
+}
+
+Console.Clear();
+
+string msg;
+
+Console.Write("Enter a number: ");
+int number = int.Parse(Console.ReadLine()!);
+
+if (IsPalindrome(number))
+{
+    msg = "a PALINDROME";
+}
+else
+{
+    msg = "NOT a palindrome";
+}
+
+Console.WriteLine($"\nNumber {number} is {msg}");
