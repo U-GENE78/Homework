447a12a1e224b91bc02de95434b57d624af8c5b8
Eugene B <e.balyabin@gmail.com>
2022-10-11 22:43:02 +0300

    task 25 added

---
+++ Lesson_4/Task_25/Task25.cs
@@ -0,0 +1,34 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную
+//степень B. Реализовать функцию возведения в степень самостоятельно!
+
+int NumToPower(int num, int pow)
+{
+
+    int result = num;
+
+    for(int i = 1; i < pow; i++)
+    {
+        result *= num;
+    }
+
+    return result;
+}
+
+
+
+Console.Clear();
+
+int number;
+int power;
+
+Console.Write("Enter a number: ");
+number = int.Parse(Console.ReadLine()!);
+
+do
+{
+    Console.Write("Enter a power: ");
+    power = int.Parse(Console.ReadLine()!);
+}
+while (power < 1);
+
+Console.WriteLine($"Number {number} to the power of {power} is {NumToPower(number, power)}\n");
