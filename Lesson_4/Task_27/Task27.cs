eb81765ae11f8f26cdb0b28cb65682b12f705403
Eugene B <e.balyabin@gmail.com>
2022-10-11 22:55:34 +0300

    task 27 added

---
+++ Lesson_4/Task_27/Task27.cs
@@ -0,0 +1,23 @@
+﻿// Напишите функцию и запустите ее, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int SumOfDigits (int num)
+{
+    int sum = 0;
+
+    num = Math.Abs(num);
+
+    while (num >= 1)
+    {
+        sum += num % 10;
+        num = num / 10;
+    }
+
+    return sum;
+}
+
+Console.Clear();
+
+Console.Write("Enter a number: ");
+int number = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"\nSum of digits in number {number} is {SumOfDigits(number)}\n");
