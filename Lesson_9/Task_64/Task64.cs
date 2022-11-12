4ecd4ce91748a9c2773441bd5b0e7c94ba8d798d
Eugene B <e.balyabin@gmail.com>
2022-11-12 22:00:07 +0300

    task 64 added

---
+++ Lesson_9/Task_64/Task64.cs
@@ -0,0 +1,21 @@
+﻿// Задайте значение N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+string NumbersDescending(int number)
+{
+    return number == 1 ? "1" : $"{number}, " + NumbersDescending(number - 1);
+}
+
+/// MAIN
+
+Console.Clear();
+
+int maxValue;
+
+Console.Write("Enter a number: ");
+do
+{
+    maxValue = int.Parse(Console.ReadLine()!);
+} while (maxValue < 1);
+
+Console.WriteLine($"{NumbersDescending(maxValue)}");
