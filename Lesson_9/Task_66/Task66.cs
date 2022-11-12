6eb33bd878f32408a851add00d7e03fe70f090a5
Eugene B <e.balyabin@gmail.com>
2022-11-12 22:19:33 +0300

    task 66 added

---
+++ Lesson_9/Task_66/Task66.cs
@@ -0,0 +1,35 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// Решение для целых чисел
+
+int SumOfNumbers(int min, int max)
+{
+    return min == max ? min : min + SumOfNumbers(min + 1, max);
+}
+
+///MAIN
+
+Console.Clear();
+
+int minValue;
+int maxValue;
+
+do
+{
+    Console.Clear();
+
+    Console.Write("Enter the minimum value: ");
+    minValue = int.Parse(Console.ReadLine()!);
+
+    Console.Write("Enter the maximum value: ");
+    maxValue = int.Parse(Console.ReadLine()!);
+
+    if (minValue > maxValue)
+    {
+        Console.WriteLine("Maximum value must be greater or equal to minimum value!");
+        Console.ReadLine();
+    }
+
+} while (minValue > maxValue);
+
+Console.WriteLine($"Sum of natural numbers from {minValue} to {maxValue} is {SumOfNumbers(minValue, maxValue)}");
