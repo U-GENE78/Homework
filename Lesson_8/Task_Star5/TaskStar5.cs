63f0f846a14bbd0677f11efbcfed5839312ce6f0
Eugene B <e.balyabin@gmail.com>
2022-11-12 10:15:56 +0300

    task star 5 added

---
+++ Lesson_8/Task_Star5/TaskStar5.cs
@@ -0,0 +1,32 @@
+﻿// Написать функцию Sqrt(x) - квадратного корня, которая принимает на вход целочисленное значение x
+// и возвращает целую часть квадратного корня от введенного числа.
+
+int SqRoot(int number)
+{
+    int sqRoot = 0;
+    int oddNumbers = 1;
+
+    while(number >= oddNumbers)
+    {
+        number -= oddNumbers;
+        sqRoot ++;
+        oddNumbers += 2;
+    }
+
+    return sqRoot;
+}
+
+///MAIN
+
+Console.Clear();
+
+int number;
+int sqRoot;
+
+Console.Write("Enter a number: ");
+do
+{
+    number = int.Parse(Console.ReadLine()!);
+} while (number < 0);
+
+Console.WriteLine($"Square root of {number} is approximately {SqRoot(number)}");
